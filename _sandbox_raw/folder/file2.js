2ded4d5f2b39616a54cf29f8d87b5a4f5867ff3a
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-07 23:43:01 -0300

    _sanbox_raw reset

--- _sandbox_raw/folder/file2.js
+++ _sandbox_raw/folder/file2.js
@@ -1,15 +1,5 @@
-var debug = require('debug')(__filename);
-var __astLoggerPrint__ = require('ast-logger-print');
 module.exports = {
   sum_inner: function(a, b) {
-    var __debug_data__ = {
-      name: 'anonymous',
-      arguments: arguments,
-      line: {original_line: 2}
-    };
-
-    __debug_data__.return_data = (a + b);
-    __astLoggerPrint__(debug, __debug_data__);
-    return __debug_data__.return_data;
+    return a + b;
   }
 };
