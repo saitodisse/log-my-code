b5c70036b87f2d0aafc08c34eeedb275697aa28b
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-21 21:57:37 -0300

    error with colors on stack trace

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 require('source-map-support').install();
 
+var chalk = require('chalk');
+var package_json = require('./package.json');
+var stack_trace_name = [
+  '/',
+  chalk.styles.gray.close,
+  chalk.bold.green(package_json.name),
+  chalk.gray('('),
+  chalk.green(package_json.version),
+  chalk.gray(')'),
+  chalk.styles.gray.open,
+].join('');
+
 var azk_gulp = require('azk-dev/gulp')({
   cwd  : __dirname,
-  sourcemaps_path: "/debug-print", // Custom path to prefix transpiled files
+  sourcemaps_path: stack_trace_name,
   lint: [ "bin/**/*.js" ], // Extra files for the lint analyzer
+  default: ['watch:test:lint'],
 });
 
 var gulp = azk_gulp.gulp;
